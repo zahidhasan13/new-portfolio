082eb2531513ce7b627c5b80b38fc2d51dee5d11
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-12-27 20:53:44 +0600

    project section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const tl = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
 
 tl.from(".row-2", {
   y: -500,
@@ -9,13 +10,15 @@ tl.from(".row-2", {
 tl.from(".line", {
   width: 0,
   duration: 1,
+  ease: "power4.out",
   stagger: 0.2,
 });
 tl.from(
-  "h2",
+  ".header h2",
   {
     y: 100,
     duration: 0.5,
+    ease: "power4.out",
     stagger: 0.2,
   },
   "-=1.7"
@@ -25,7 +28,64 @@ tl.from(
   {
     scale: 0,
     duration: 0.5,
+    ease: "power4.out",
     stagger: 0.2,
   },
   "-=0.5"
 );
+
+const innerCursor = document.querySelector(".custom-cursor--inner");
+const outerCursor = document.querySelector(".custom-cursor--outer");
+
+document.addEventListener("mousemove", (event) => {
+  const { clientX: x, clientY: y } = event;
+
+  gsap.to(innerCursor, {
+    x,
+    y,
+    duration: 0.1,
+  });
+  gsap.to(outerCursor, {
+    x,
+    y,
+    duration: 0.2,
+    delay: 0.05,
+  });
+});
+
+// Add hover effect
+document.querySelectorAll("a, button").forEach((element) => {
+  element.addEventListener("mouseenter", () => {
+    gsap.to(outerCursor, {
+      scale: 1.5,
+      duration: 0.3,
+    });
+  });
+  element.addEventListener("mouseleave", () => {
+    gsap.to(outerCursor, {
+      scale: 1,
+      duration: 0.3,
+    });
+  });
+});
+
+// Marque
+window.addEventListener("wheel", (e) => {
+  if (e.deltaY > 0) {
+    gsap.to(".marque", {
+      transform: "translateX(-400%)",
+      repeat: -1,
+      duration: 6,
+      ease: "none",
+    });
+  } else {
+    gsap.to(".marque", {
+      transform: "translateX(0%)",
+      repeat: -1,
+      duration: 6,
+      ease: "none",
+    });
+  }
+});
+
+// Projects Section
