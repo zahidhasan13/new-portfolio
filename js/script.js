67542b88e11e95ffab714770d61ba493186c66e0
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-12-28 12:50:21 +0600

    about and skill section

--- js/script.js
+++ js/script.js
@@ -89,3 +89,124 @@ window.addEventListener("wheel", (e) => {
 });
 
 // Projects Section
+gsap.from(".project-wrapper hr", {
+  width: 0,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#projects",
+    start: "top 30%",
+  },
+});
+gsap.from(".project-titles span", {
+  y: -500,
+  duration: 1,
+  delay: 0.5,
+  ease: "power4.out",
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#projects",
+    start: "top 30%",
+  },
+});
+gsap.from(".project-titles h2", {
+  y: 500,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#projects",
+    start: "top 30%",
+  },
+});
+gsap.from(
+  ".project-details span",
+  {
+    y: 500,
+    duration: 1,
+    ease: "power4.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: "#projects",
+      start: "top 30%",
+    },
+  },
+  "-=2"
+);
+
+const projectItems = document.querySelectorAll(".project-item");
+
+projectItems.forEach((item) => {
+  const hoverImage = document.createElement("div");
+  hoverImage.classList.add("hover-image");
+  item.appendChild(hoverImage);
+
+  item.addEventListener("mouseenter", () => {
+    const imageUrl = item.getAttribute("data-image");
+    if (imageUrl) {
+      hoverImage.style.backgroundImage = `url(${imageUrl})`;
+
+      gsap.to(hoverImage, {
+        opacity: 1,
+        scale: 1,
+        duration: 0.3,
+        ease: "power2.out",
+      });
+    }
+  });
+
+  item.addEventListener("mousemove", (event) => {
+    const { offsetX } = event;
+
+    gsap.to(hoverImage, {
+      left: offsetX + 20 + "px",
+      duration: 0.2,
+    });
+  });
+
+  item.addEventListener("mouseleave", () => {
+    gsap.to(hoverImage, {
+      opacity: 0,
+      scale: 0.8,
+      duration: 0.3,
+      ease: "power2.in",
+    });
+  });
+});
+
+// SKills section
+gsap.fromTo(
+  ".skills-left .progress-fill",
+  {
+    width: 0, // Start at 0
+  },
+  {
+    width: (index, target) => target.getAttribute("data-progress") + "%",
+    duration: 1.5,
+    ease: "power4.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: "#skills",
+      start: "top 30%",
+    },
+  }
+);
+
+// Animate Right Side Progress Bars
+gsap.fromTo(
+  ".skills-right .progress-fill",
+  {
+    width: 0, // Start at 0
+  },
+  {
+    width: (index, target) => target.getAttribute("data-progress") + "%",
+    duration: 1.5,
+    ease: "power4.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: "#skills",
+      start: "top 30%",
+    },
+  }
+);
